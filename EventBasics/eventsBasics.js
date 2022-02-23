fe63a910d0503301ff5aa466844f55d647057048
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-23 16:17:53 -0600

    -Edited README.md. EVENTS --> addEventListener

---
+++ EventBasics/eventsBasics.js
@@ -0,0 +1,41 @@
+//EVENTS ON JS ****************************************************************************
+const btn = document.querySelector("#v2");
+
+btn.onclick = function () {
+    console.log("YOU CLICKED ME! \nI HOPE IT WORKED!! ")
+    //console.log("I HOPE IT WORKED!!")
+}
+
+function scream() {
+    console.log("AAAAAHHHHH");
+    console.log("STOP TOUCHING ME!")
+}
+
+btn.onmouseenter = scream;
+
+
+document.querySelector("h1").onclick = () => {
+    alert("you clicked the h1!")
+}
+
+//ADDEVENTLISTENER
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function () {
+    alert("CLICKED!");
+});
+
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector('#tas');
+
+//this will override twists and only shout will be executed
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist, {once: "true"});
+tasButton.addEventListener('click', shout);
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ## DOM EVENTS
 -Events
+    *Responding to user inputs and actions like
+        -clicks             -drags          -drops          -hovers         -scrolls    
+        -form submission    -key presses    -focus/blur     -mouse wheel    -double click
+        -copying            -pasting        -audio start    -screen resize  -printing        
 -addEventListener
 -Forms Events
 -Keyboard Events
