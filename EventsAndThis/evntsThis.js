d29a79270d2df10ce81f6f0ee6eaf1fc7692e6aa
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-23 19:18:17 -0600

    Events and the keyword 'this'

---
+++ EventsAndThis/evntsThis.js
@@ -0,0 +1,23 @@
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function colorize() {
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
+}
+
+
+const buttons = document.querySelectorAll("button");
+for (let button of buttons) {
+    button.addEventListener("mouseover", colorize)
+}
+
+const h1s = document.querySelectorAll("h1");
+for (let h1 of h1s) {
+    h1.addEventListener("mouseover", colorize);
+}
+
